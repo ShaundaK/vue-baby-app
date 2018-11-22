983668bc5095f74eb9dbb8478c2023011e48f9ba
ShaundaK <sdking1913@gmail.com>
2018-11-21 20:42:44 -0600

    product in vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <p>name: {{ product.name }}</p>
+      <p>description: {{product.description }}</p>
+      <p>age: {{ product.age }}</p>
+      <p>gender: {{ product.gender }}</p>
+    </div>
   </div>
 </template>
 
@@ -8,13 +14,22 @@
 </style>
 
 <script>
+var axios = require('axios');
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      products: [],
     };
   },
-  created: function() {},
+  created: function() {
+    console.log(this)
+    axios.get('http://localhost:3000/api/products').then(function(response) {
+      console.log(response.data);
+      this.products = response.data
+    }.bind(this));
+  },
+
   methods: {},
   computed: {}
 };
