bfde67e3d93a21b5aaabd202dd2b12296fb6fe3a
ShaundaK <sdking1913@gmail.com>
2018-11-28 18:59:54 -0600

    login page

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Category from './views/Category.vue'
 import Signup from './views/Signup.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
@@ -30,6 +31,11 @@ export default new Router({
       path: '/signup',
       name: 'signup',
       component: Signup
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
